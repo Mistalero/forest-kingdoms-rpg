7162ed11eaf5f30a26f184ceb7812dc96c3ae8a6
catman78 <catman78@sourcecraft.dev>
2025-11-10 10:03:34 +0000

    ...

---
+++ src/scripts/Main.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+# Основной скрипт игры Forest Kingdoms RPG
+
+func _ready():
+	print("Forest Kingdoms RPG - Игра загружена")
+	print("Выберите фракцию для начала игры:")
+	print("1. Лесные эльфы")
+	print("2. Охрана дворца")
+	print("3. Злодеи")
+
+func _process(delta):
+	pass
